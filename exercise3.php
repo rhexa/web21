b5e9ad93025e2cb326aba287e4b574140cc58a95
rhexa <akun.kripitit@gmail.com>
2021-01-29 15:19:38 +0200

    done 1st question

---
+++ exercise3.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <style>
+        h3 {
+            background-color: #a4dfef;
+        }
+    </style>
+
+    <title>Exercise 3</title>
+</head>
+<body>
+
+    <div class="container">
+        <!-- 1 -->
+        <article>
+            <h3>1. Write a script that gets the current month and prints one of the following responses, depending on whether it's August or not:</h3>
+            <?php
+                if (date(n) == 8) {
+                    echo "<p>It's " . date(F) . ", so it's still holiday</p>";
+                }
+                else {
+                    echo "<p>Not August, This is " . date(F) . " so i don't have any holidays</p>";
+                }
+            ?>
+        </article>
+
+        <!-- 2 -->
+        <article>
+            <h3>2. Assign color red to a variable name $color and check to print one the following responses using if else statement </h3>
+            <p></p>
+        </article>
+
+        <!-- 3 -->
+        <article>
+            <h3>3. Write a program to grade students based on their total score for a subject. The grading scheme is: Excellent : >80 ;Great >70 & less than 80;Good >60 & less than 70; Pass >50 & less than 60 & Fail <50</h3>
+            <p></p>
+        </article>
+
+        <!-- 4 -->
+        <article>
+            <h3>4. Write a program to get inputs (age and name) from the user and based on their age, decide if he/she is eligible for voting. (18 or more than 18 years is eligible for voting.)</h3>
+            <p></p>
+        </article>
+
+        <!-- 5 -->
+        <article>
+            <h3>5. Use a looping statement to construct the following pattern: </h3>
+            <p></p>
+        </article>
+
+        <!-- 6 -->
+        <article>
+            <h3>6. Use While loop to print the following pattern: </h3>
+            <p></p>
+        </article>
+
+        <!-- 7 -->
+        <article>
+            <h3>7. Create a GitHub repo and enable GitHub pages for the repo. Upload an HTML file of yours to the repo. You should include in your PHP code the link to the repo and your web page. </h3>
+            <p></p>
+        </article>
+    </div>
+
+
+
+<!-- bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</body>
+</html>
