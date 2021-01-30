63dfa02a5e9b8d12c454d8f3ecdacd9fc4275638
rhexa <akun.kripitit@gmail.com>
2021-01-30 09:11:03 +0200

    done number 2

--- exercise3.php
+++ exercise3.php
@@ -8,8 +8,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     <style>
-        h3 {
+        h1 {
+            font-size: 28px;
+        }
+        h2 {
+            font-size: 20px;
             background-color: #a4dfef;
+            margin: 15px 0 0 0;
+        }
+        h3 {
+            font-size: 18px;
+            margin: 0;
+        }
+        div.answer {
+            font-size: 20px;
+            background-color: #cae2fa;
+            font-weight: bold;
+            margin-bottom: 0;
+
+        }
+        p {
+            font-size: 18px;
+            margin: 0;
         }
     </style>
 
@@ -20,7 +40,13 @@
     <div class="container">
         <!-- 1 -->
         <article>
-            <h3>1. Write a script that gets the current month and prints one of the following responses, depending on whether it's August or not:</h3>
+            <h2>1. Write a script that gets the current month and prints one of the following responses, depending on whether it's August or not:</h2>
+            <p>It's August, so it's still holiday. <br>
+            Not August, This is Month-name so i don't have any holidays</p>
+            
+            <!-- answer -->
+            <div class="answer">Answer</div>
+
             <?php
                 if (date(n) == 8) {
                     echo "<p>It's " . date(F) . ", so it's still holiday</p>";
@@ -33,37 +59,66 @@
 
         <!-- 2 -->
         <article>
-            <h3>2. Assign color red to a variable name $color and check to print one the following responses using if else statement </h3>
-            <p></p>
+            <h2>2. Assign color red to a variable name $color and check to print one the following responses using if else statement </h2>
+            <p>The color is red. <br>
+            The color is not red.</p>
+            
+            <!-- answer -->
+            <div class="answer">Answer</div>
+            <?php
+                $color = red;
+
+                if ($color == "red") {
+                    echo "<p>The color is red.</p>";
+                }
+                else {
+                    echo "<p>The color is not red</p>";
+                }
+            ?>
         </article>
 
         <!-- 3 -->
         <article>
-            <h3>3. Write a program to grade students based on their total score for a subject. The grading scheme is: Excellent : >80 ;Great >70 & less than 80;Good >60 & less than 70; Pass >50 & less than 60 & Fail <50</h3>
+            <h2>3. Write a program to grade students based on their total score for a subject. The grading scheme is: Excellent : >80 ;Great >70 & less than 80;Good >60 & less than 70; Pass >50 & less than 60 & Fail <50</h2>
+            
+            <!-- answer -->
+            <div class="answer">Answer</div>
             <p></p>
         </article>
 
         <!-- 4 -->
         <article>
-            <h3>4. Write a program to get inputs (age and name) from the user and based on their age, decide if he/she is eligible for voting. (18 or more than 18 years is eligible for voting.)</h3>
+            <h2>4. Write a program to get inputs (age and name) from the user and based on their age, decide if he/she is eligible for voting. (18 or more than 18 years is eligible for voting.)</h2>
+            
+            <!-- answer -->
+            <div class="answer">Answer</div>
             <p></p>
         </article>
 
         <!-- 5 -->
         <article>
-            <h3>5. Use a looping statement to construct the following pattern: </h3>
+            <h2>5. Use a looping statement to construct the following pattern: </h2>
+       
+            <!-- answer -->
+            <div class="answer">Answer</div>
             <p></p>
         </article>
 
         <!-- 6 -->
         <article>
-            <h3>6. Use While loop to print the following pattern: </h3>
+            <h2>6. Use While loop to print the following pattern: </h2>
+            
+            <!-- answer -->
+            <div class="answer">Answer</div>
             <p></p>
         </article>
 
         <!-- 7 -->
         <article>
-            <h3>7. Create a GitHub repo and enable GitHub pages for the repo. Upload an HTML file of yours to the repo. You should include in your PHP code the link to the repo and your web page. </h3>
+            <h2>7. Create a GitHub repo and enable GitHub pages for the repo. Upload an HTML file of yours to the repo. You should include in your PHP code the link to the repo and your web page. </h2>
+           
+            <!-- answer -->
+            <div class="answer">Answer</div>
             <p></p>
         </article>
     </div>
